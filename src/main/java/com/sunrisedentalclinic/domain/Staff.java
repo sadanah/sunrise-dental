40df4c4c6bbf3e718346aa7942e4b97ef568882b
Sadana Herath <sadana.herath@gmail.com>
2026-07-27 21:52:55 +0530

    staff (sub) abstract class

---
+++ src/main/java/com/sunrisedentalclinic/domain/Staff.java
@@ -0,0 +1,31 @@
+package com.sunrisedentalclinic.domain;
+
+public abstract class Staff extends Person {
+    private String staffID;
+    private String username;
+    private String passwordHash;
+
+    public Staff(int personID, String name, String contactNo, String address,
+                 String staffID, String username, String passwordHash) {
+        super(personID, name, contactNo, address);
+        this.staffID = staffID;
+        this.username = username;
+        this.passwordHash = passwordHash;
+    }
+
+    public abstract String getRole();
+
+    @Override
+    public String getDetails() {
+        return getRole() + ": " + name + " (Staff ID: " + staffID + ")";
+    }
+
+    public String getstaffID() { return staffID; }
+    public void setstaffID(String staffID) { this.staffID = staffID; }
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+    public String getPasswordHash() { return passwordHash; }
+    public void setPasswordHash(String passwordHash) { this.passwordHash = passwordHash; }
+
+    // login and logout to be implemented later
+}
