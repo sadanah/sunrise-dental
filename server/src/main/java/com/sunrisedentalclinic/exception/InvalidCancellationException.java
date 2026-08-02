d9daf74e86f4f728e6dc218b9f6ffb26f0148080
Sadana Herath <sadana.herath@gmail.com>
2026-08-02 21:17:04 +0530

    Add domain exceptions

---
+++ server/src/main/java/com/sunrisedentalclinic/exception/InvalidCancellationException.java
@@ -0,0 +1,7 @@
+package com.sunrisedentalclinic.exception;
+
+public class InvalidCancellationException extends RuntimeException {
+    public InvalidCancellationException(String message) {
+        super(message);
+    }
+}
