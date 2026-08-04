ed19dadb1438d906f97dc8c3799526bf2273f21c
Sadana Herath <sadana.herath@gmail.com>
2026-08-04 21:21:17 +0530

    Added help panel with help topics to all dashboards.

--- client/src/main/java/com/sunrisedentalclinic/client/ui/dashboard/ReceptionistDashboardFrame.java
+++ client/src/main/java/com/sunrisedentalclinic/client/ui/dashboard/ReceptionistDashboardFrame.java
@@ -5,6 +5,7 @@ import com.sunrisedentalclinic.client.AppSession;
 import com.sunrisedentalclinic.client.ui.LoginFrame;
 import com.sunrisedentalclinic.client.ui.components.SidebarPanel;
 import com.sunrisedentalclinic.client.ui.panels.receptionist.HomePanel;
+import com.sunrisedentalclinic.client.ui.panels.HelpPanel;
 import com.sunrisedentalclinic.client.ui.panels.receptionist.RegisterPatientPanel;
 import com.sunrisedentalclinic.client.ui.panels.receptionist.SearchPatientsPanel;
 import com.sunrisedentalclinic.client.ui.panels.receptionist.RegisterAppointmentPanel;
@@ -23,6 +24,7 @@ public class ReceptionistDashboardFrame extends JFrame {
     private static final String GENERATE_BILL = "GENERATE_BILL";
     private static final String REGISTER_PATIENT = "REGISTER_PATIENT";
     private static final String SEARCH_PATIENT = "SEARCH_PATIENT";
+    private static final String HELP = "HELP";
 
     private final ApiClient apiClient;
     private final CardLayout cardLayout = new CardLayout();
@@ -46,6 +48,7 @@ public class ReceptionistDashboardFrame extends JFrame {
         sidebar.addNavButton("Register", null, REGISTER_PATIENT, this::navigate);
         sidebar.addNavButton("Search", null, SEARCH_PATIENT, this::navigate);
         sidebar.addGlue();
+        sidebar.addNavButton("Help", null, HELP, this::navigate);
         sidebar.addNavButton("Logout", IconLoader.load("logout.png"), "LOGOUT", this::navigate);
 
         contentPanel.add(new HomePanel(), HOME);
@@ -54,6 +57,7 @@ public class ReceptionistDashboardFrame extends JFrame {
         contentPanel.add(new GenerateBillPanel(apiClient), GENERATE_BILL);
         contentPanel.add(new RegisterPatientPanel(apiClient), REGISTER_PATIENT);
         contentPanel.add(new SearchPatientsPanel(apiClient), SEARCH_PATIENT);
+        contentPanel.add(new HelpPanel(apiClient), HELP);
 
         add(sidebar, BorderLayout.WEST);
         add(contentPanel, BorderLayout.CENTER);
