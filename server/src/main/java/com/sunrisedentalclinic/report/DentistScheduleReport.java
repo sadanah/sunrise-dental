b2784ed717c7023d0f81229d5b34852f30b5f13c
Sadana Herath <sadana.herath@gmail.com>
2026-08-06 18:53:44 +0530

    Build error for date and time method names fixed

--- server/src/main/java/com/sunrisedentalclinic/report/DentistScheduleReport.java
+++ server/src/main/java/com/sunrisedentalclinic/report/DentistScheduleReport.java
@@ -11,21 +11,27 @@ public class DentistScheduleReport extends Report {
     private final String dentistID;
     private final AppointmentDAO appointmentDAO;
     private List<Appointment> appointments;
+    private final LocalDate reportDate;
 
     public DentistScheduleReport(
             String reportID,
             String generatedBy,
             String dentistID,
-            LocalDate startDate, AppointmentDAO appointmentDAO) {
+            LocalDate reportDate,
+            AppointmentDAO appointmentDAO) {
 
         super(reportID, generatedBy);
         this.dentistID = dentistID;
+        this.reportDate = reportDate;
         this.appointmentDAO = appointmentDAO;
     }
 
     @Override
     public void generate() {
-        appointments = appointmentDAO.findByDentist(dentistID);
+        appointments = appointmentDAO.findByDentistAndDate(
+                dentistID,
+                reportDate
+        );
     }
 
     public List<Appointment> getAppointments() {
